b0d41570c4e94f1e3bc3f67e22ad5d812961bc99
Aishwarya Pratap Singh <pratapsingh.aishwarya@hotmail.com>
2016-09-29 23:34:54 -0700

    Initial Commit

---
+++ APSGroupedTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APSGroupedTableView
+//
+//  Created by Aishwarya Pratap Singh on 9/29/16.
+//  Copyright © 2016 Aishwarya Pratap Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
